f8f121a1fdd9f530e6a601a458c7eccc1006de8a
Rehan Hamayun <rr8329203@gmail.com>
2022-07-28 15:40:04 +0500

    read changes fixed

--- lib/View/home_page.dart
+++ lib/View/home_page.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -11,10 +10,9 @@ import 'package:shakespear_app/Provider_Controllers/favorite_quote_provider.dart
 import 'package:shakespear_app/Provider_Controllers/provider_saver.dart';
 import 'package:shakespear_app/Provider_Controllers/recent_quotes_provider.dart';
 import 'package:shakespear_app/View/drawer.dart';
-
+import 'package:shakespear_app/View/poetry_max.dart';
 import 'package:shakespear_app/configs/configs.dart';
 import 'package:sizer/sizer.dart';
-
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -50,15 +48,10 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       backgroundColor: Colors.white.withOpacity(0.9),
-
       appBar: AppBar(
         backgroundColor: Colors.grey.withOpacity(0),
         elevation: 0.0,
         iconTheme: IconThemeData(color: Colors.black),
-
-
-
-       
 
         //centerTitle: true,
       ),
@@ -66,34 +59,38 @@ class _HomePageState extends State<HomePage> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: 30,),
+          SizedBox(
+            height: 30,
+          ),
           Padding(
-            padding: const EdgeInsets.only(left:22),
+            padding: const EdgeInsets.only(left: 22),
             child: Row(
-
               children: [
                 Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Shake Speare Quotes", style: GoogleFonts.poppins(
-                        color: Colors.black,
-                        fontSize: 19,
-                        fontWeight: FontWeight.bold
-                    ),),
-                    SizedBox(height: 9,),
-                    Text("Total Quotes: 150", style: GoogleFonts.poppins(
-                      color: Colors.black.withOpacity(0.5),
-                      fontSize: 12,
-                    ),),
-
-
+                    Text(
+                      "Shake Speare Quotes",
+                      style: GoogleFonts.poppins(
+                          color: Colors.black,
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 9,
+                    ),
+                    Text(
+                      "Total Quotes: 150",
+                      style: GoogleFonts.poppins(
+                        color: Colors.black.withOpacity(0.5),
+                        fontSize: 12,
+                      ),
+                    ),
                   ],
-
                 ),
-
                 Padding(
-                  padding: const EdgeInsets.only(left:50.0),
+                  padding: const EdgeInsets.only(left: 50.0),
                   child: Container(
                     decoration: BoxDecoration(
                       color: Colors.red,
@@ -104,143 +101,169 @@ class _HomePageState extends State<HomePage> {
                     child: CircleAvatar(
                       backgroundImage: NetworkImage(userImage, scale: 1),
                     ),
-
                   ),
                 ),
-
-
-
-
               ],
-
             ),
           ),
           FutureBuilder(
-            future: getPoetryApi(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return Center(
-                    child: CircularProgressIndicator(
-                      //backgroundColor: Colors.black,
-                      color: Colors.black,
-                      strokeWidth: 6,
+              future: getPoetryApi(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                      child: CircularProgressIndicator(
+                    //backgroundColor: Colors.black,
+                    color: Colors.black,
+                    strokeWidth: 6,
+                  ));
+                } else {
+                  return Expanded(
+                      child: ListView.builder(
+                          itemCount: poetryList.length,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Container(
+                                height: 17.h,
+                                width: 50.w,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
 
+                                // SubString method to remove first few digits from the fetch data
+                                //It removes first 9 digits of fetch data in List String.
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.fromLTRB(
+                                                0, 5, 18, 0),
+                                            child: GestureDetector(
+                                              onTap: () {
+                                                favorite.addFavoriteQuote(
+                                                    poetryList[index]
+                                                        .title
+                                                        .toString());
+                                              },
+                                              child: Container(
 
-                    ));
-              } else {
-                return Expanded(
-                    child: ListView.builder(itemCount: poetryList.length,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Container(
-                              height: 17.h,
-                              width: 50.w,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-
-                              // SubString method to remove first few digits from the fetch data
-                              //It removes first 9 digits of fetch data in List String.
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-
-                                children: [
-                                  Container(
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        Padding(
-                                          padding: const EdgeInsets.fromLTRB(0,5,18,0),
-                                          child: GestureDetector(
-                                            onTap: () {
-                                              favorite.addFavoriteQuote(
-                                                  poetryList[index].title
-                                                      .toString());},
-
-
-                                              child:
-                                              Container(
-
-                                                // Favorite Button Logic added.
+                                                  // Favorite Button Logic added.
                                                   child: favorite.favoriteQuote
-                                                      .contains(
-                                                      poetryList[index].title)
+                                                          .contains(
+                                                              poetryList[index]
+                                                                  .title)
                                                       ? likeIcon
                                                       : unlikeIcon),
-
                                             ),
-                                        ),
+                                          ),
                                         ],
+                                      ),
                                     ),
-                                  ),
-                                  SizedBox(height: 5,),
-
-                                     Padding(
-                                       padding: const EdgeInsets.only(left:8.0),
-                                       child: Text(poetryList[index].title.toString().substring(10).replaceAll(":", ""), style: GoogleFonts.poppins(
-                                        color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold,
-                                    ),),
-                                     ),
-                                  SizedBox(height: 4,),
-
-                                  Padding(
-                                    padding: const EdgeInsets.only(left:20.0),
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      children: [
-                                        Text("Author:", style: GoogleFonts.poppins(
-                                          color: Colors.black.withOpacity(0.5), fontSize: 9,
-                                        ),),
-                                        SizedBox(width: 8,),
-                                        Text(poetryList[index].author.toString(), style: GoogleFonts.poppins(
-                                          fontSize: 11, fontWeight: FontWeight.w500, color: Colors.black.withOpacity(0.7),
-                                        ),),
-                                      ],
+                                    SizedBox(
+                                      height: 12,
                                     ),
-                                  ),
-                                  SizedBox(height: 8,),
-
-                                  GestureDetector(
-                                    onTap: (){},
-                                    child: Container(
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(right:20.0),
-                                        child: Row(
-                                          mainAxisAlignment: MainAxisAlignment.end,
-                                          children: [
-
-                                            Text("Read", style: GoogleFonts.poppins(
-                                              color: Colors.black.withOpacity(0.6),
-                                              fontWeight: FontWeight.w600, fontSize: 12,
-                                            ), ),
-                                            SizedBox(width: 5,),
-                                            Icon(CupertinoIcons.book, size: 12,),
-
-
-                                          ],
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: Text(
+                                        poetryList[index]
+                                            .title
+                                            .toString()
+                                            .substring(10)
+                                            .replaceAll(":", ""),
+                                        style: GoogleFonts.poppins(
+                                          color: Colors.black,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold,
                                         ),
                                       ),
                                     ),
-                                  ),
-
-
-                                ],
+                                    SizedBox(
+                                      height: 4,
+                                    ),
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.only(left: 20.0),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            "Author:",
+                                            style: GoogleFonts.poppins(
+                                              color:
+                                                  Colors.black.withOpacity(0.5),
+                                              fontSize: 9,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 8,
+                                          ),
+                                          Text(
+                                            poetryList[index].author.toString(),
+                                            style: GoogleFonts.poppins(
+                                              fontSize: 11,
+                                              fontWeight: FontWeight.w500,
+                                              color:
+                                                  Colors.black.withOpacity(0.7),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    GestureDetector(
+                                      onTap: () {
+                                        Navigator.push(context, MaterialPageRoute(builder: (context)=> PoetryMax(shakeModel: poetryList[index])));
+                                      },
+                                      child: Container(
+                                        child: Padding(
+                                          padding: const EdgeInsets.only(
+                                              right: 20.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            children: [
+                                              Text(
+                                                "Read",
+                                                style: GoogleFonts.poppins(
+                                                  color: Colors.black
+                                                      .withOpacity(0.6),
+                                                  fontWeight: FontWeight.w600,
+                                                  fontSize: 12,
+                                                ),
+                                              ),
+                                              SizedBox(
+                                                width: 5,
+                                              ),
+                                              Icon(
+                                                CupertinoIcons.book,
+                                                size: 12,
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                          );
-                        }
-                    ));
-              }
+                            );
+                          }));
+                }
 
-
-              // ElevatedButton(onPressed: () {}, child: Text("=>"))
-            })],
+                // ElevatedButton(onPressed: () {}, child: Text("=>"))
+              })
+        ],
       ),
       drawer: MyDrawer(),
     );
   }
 }
-
--- lib/View/poetry_max.dart
+++ lib/View/poetry_max.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:shakespear_app/Models/poetry_model.dart';
 
 import 'package:shakespear_app/Provider_Controllers/provider_saver.dart';
 
 class PoetryMax extends StatelessWidget {
-  PoetryMax({Key? key}) : super(key: key);
+  final ShakeModel shakeModel;
+  PoetryMax({Key? key, required this.shakeModel}) : super(key: key);
 
 // WE SHOW THE SAVED LISTS OF LINES OF THAT PARTICULAR TITLE THERE.
 // i.e lister.savedlines[index] and the logic is in provider_saver.dart
@@ -14,21 +17,18 @@ class PoetryMax extends StatelessWidget {
     final lister = Provider.of<ShowLines>(context);
     return Scaffold(
       appBar: AppBar(
+        title: Text(shakeModel.title.toString().substring(10).replaceAll(":", "")),
         backgroundColor: Colors.black87,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: lister.savedLines.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: Text(lister.savedLines[index].toString()),
-                );
-              },
-            ),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Text(shakeModel.lines.toString(), style: GoogleFonts.poppins(
+              color: Colors.black, fontSize: 14,
+            ),),
+          ],
+        ),
       ),
     );
   }
